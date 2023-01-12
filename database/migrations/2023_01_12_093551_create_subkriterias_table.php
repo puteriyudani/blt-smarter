5eb02bca3539fae92d63d4ca642c83898227f5aa
Puteri Yudani <puteriyudani@gmail.com>
2023-01-12 16:38:01 +0700

    database

---
+++ database/migrations/2023_01_12_093551_create_subkriterias_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subkriterias', function (Blueprint $table) {
+            $table->id('subkriteria_id');
+            $table->integer('kriteria_id');
+            $table->string('nama');
+            $table->integer('prioritas');
+            $table->float('bobot');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subkriterias');
+    }
+};
