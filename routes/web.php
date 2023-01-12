e8d1602b12bffd84a7a8c6f647101ea7949cb2df
Puteri Yudani <puteriyudani@gmail.com>
2023-01-12 16:55:11 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\MasyarakatController;
+use App\Http\Controllers\KriteriaController;
+use App\Http\Controllers\PenilaianController;
+use App\Http\Controllers\SubkriteriaController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +19,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UserController::class, 'login'])->name('login');
+
+Route::get('beranda', [BerandaController::class, 'index'])->name('beranda');
+
+// user
+Route::get('register', [UserController::class, 'register'])->name('register');
+Route::post('register', [UserController::class, 'register_action'])->name('register.action');
+Route::post('login', [UserController::class, 'login_action'])->name('login.action');
+Route::get('password', [UserController::class, 'password'])->name('password');
+Route::post('password', [UserController::class, 'password_action'])->name('password.action');
+Route::get('logout', [UserController::class, 'logout'])->name('logout');
+
+// masyarakat
+Route::resource('masyarakats', MasyarakatController::class);
+Route::resource('penilaian', PenilaianController::class);
+
+//kriteria
+Route::resource('kriterias', KriteriaController::class);
+Route::resource('subkriterias', SubkriteriaController::class);
