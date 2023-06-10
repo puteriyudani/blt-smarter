52d9bc75c2a9ed4f1c594b9e2ac8169c8b812102
Puteri Yudani <puteriyudani@gmail.com>
2023-06-10 15:55:28 +0700

    halaman awal

--- routes/web.php
+++ routes/web.php
@@ -27,11 +27,14 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Auth::routes();
-Route::get('/', [UserController::class, 'login'])->name('login');
+Route::get('/', function () {
+    return view('home');
+ });
 
 // login & register
 Route::get('register', [UserController::class, 'register'])->name('register');
 Route::post('register', [UserController::class, 'register_action'])->name('register.action');
+Route::get('login', [UserController::class, 'login'])->name('login');
 Route::post('login', [UserController::class, 'login_action'])->name('login.action');
 Route::get('password', [UserController::class, 'password'])->name('password');
 Route::post('password', [UserController::class, 'password_action'])->name('password.action');
